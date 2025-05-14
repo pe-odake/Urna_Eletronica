b880ca41b740a08f8fc40b6c6f2f0212fe2211c9
Pedro Odake <pedroodake@gmail.com>
2025-05-14 14:46:23 -0300

    base finalizada, faltando diferenciais, como exportar em .txt, imagem, conectar a um bd, exemplos

--- minha_urna.py
+++ minha_urna.py
@@ -1,84 +1,3 @@
-# # Desenvolva seu código aqui!
- 
-# candidatos = {
-#     'Python Silva': {'partido': 'Partido do Código Aberto (PCA)', 'numero': 11},
-#     'CSS Cardoso': {'partido': 'Desenvolvedores Ágeis (DA)', 'numero': 22},
-#     'SQL Ferreira': {'partido': 'Orientados a Objetos (POO)', 'numero': 33},
-#     'HTML Santos': {'partido': 'Marcação para Todos (MT)', 'numero': 44},
-#     'JavaScript Oliveira': {'partido': 'Assíncronos Independentes (AI)', 'numero': 55}
-# }
-# votos = {
-#     'Python Silva': 0,
-#     'CSS Cardoso': 0,
-#     'SQL Ferreira': 0,
-#     'HTML Santos': 0,
-#     'JavaScript Oliveira': 0
-# }
-
-
-# # votos = {
-# #     'Python Silva': 10,
-# #     'CSS Cardoso': 50,
-# #     'SQL Ferreira': 20,
-# #     'HTML Santos': 70,
-# #     'JavaScript Oliveira': 0
-# # }
-
-
-# # ----------------- FUNÇÕES ---------------------- #
-
-# def votar():
-#     cont = 0
-#     numeros_candidatos = []
-#     for nome, info in candidatos.items():
-#         numeros_candidatos.append(info.get("numero"))
-#         print(f'{info.get("numero")} - Candidato {nome}, partido {info.get("partido")}')
-#     while cont < 1:
-#         selec_voto = int(input('Digite o número do candidato que deseja votar'))
-#         if selec_voto in numeros_candidatos:
-#             for nome, info in candidatos.items():
-#                 if info.get('numero') == selec_voto:
-#                     print(f' Candidato: {nome}\n Número: {info.get('numero')}\n Partido: {info.get('partido')}')
-#                     confirma = input('Deseja confirmar seu voto? Digite Sim ou Não').lower()
-#                     if confirma == 'sim':
-#                         votos[nome] += 1
-#                         cont += 1
-#                         print('Voto realizado')
-#                     else: 
-#                         print('Votação reiniciada.')
-#         else: 
-#             print('Número de candidato inexistente')
-
-# def encerrar():
-#     # encerrar vai para a votação, retornar o candidato com maior numero de votos
-#     eleito = max(votos, key = votos.get)
-#     print(f'O vendedor da eleição é {eleito}, com {votos.get(eleito)} votos.')
-    
-            
-
-
-# # ------------------------------------------------ #
-# menu = """
-
-#     URNA ELETRÔNICA
-#     1 - Votar
-#     2 - Encerrar Votação
-    
-# """
-
-# while True:
-#     print(menu)
-#     opcao = input('Selecione uma opção: ')
-#     if opcao == '1':
-#         votar()
-#         continue
-#     elif opcao == '2':
-#         encerrar()
-#         break
-#     else:
-#         print('Opção invalida, selecione uma correta')
-#         continue
-
 import tkinter as tk
 from tkinter import messagebox
 
@@ -87,11 +6,10 @@ votacao_ativa = False
 candidatos = {
     'Python Silva': {'partido': 'Partido do Código Aberto (PCA)', 'numero': 11, 'voto': 0},
     'CSS Cardoso': {'partido': 'Desenvolvedores Ágeis (DA)', 'numero': 22, 'voto': 0},
-    # 'SQL Ferreira': {'partido': 'Orientados a Objetos (POO)', 'numero': 33, 'voto': 0},
-    # 'HTML Santos': {'partido': 'Marcação para Todos (MT)', 'numero': 44, 'voto': 0},
-    # 'JavaScript Oliveira': {'partido': 'Assíncronos Independentes (AI)', 'numero': 55, 'voto': 0}
+    'SQL Ferreira': {'partido': 'Orientados a Objetos (POO)', 'numero': 33, 'voto': 0},
+    'HTML Santos': {'partido': 'Marcação para Todos (MT)', 'numero': 44, 'voto': 0},
+    'JavaScript Oliveira': {'partido': 'Assíncronos Independentes (AI)', 'numero': 55, 'voto': 0}
 }
-votos = []
 
 janela = tk.Tk()
 janela.title("Dizer Olá")
@@ -107,16 +25,13 @@ def menu():
     botao_cadastro.pack(pady=5) # Espaçamento entre os botões
     botao_votacao = tk.Button(janela, text="Iniciar Votação", command=votar)
     botao_votacao.pack(pady=5)
-    botao_encerrar = tk.Button(janela, text="Encerrar Votação", command=votar)
+    botao_encerrar = tk.Button(janela, text="Encerrar Votação", command=imprimir_relatorio)
     botao_encerrar.pack(pady=5)
 
 def add_candidato():
     janela_add_candidato = tk.Toplevel(janela)
     janela_add_candidato.title("Cadastro de Candidato")
     janela_add_candidato.geometry("1200x600")
-    # BOTÃO DE VOLTAR
-    btn_voltar = tk.Button(janela_add_candidato, text="voltar", command=janela_add_candidato.destroy)
-    btn_voltar.pack(pady=10)
     # NOME CANDIDATO
     tk.Label(janela_add_candidato, text="Nome do Candidato: ").pack(pady=5)
     entrada_nome = tk.Entry(janela_add_candidato)
@@ -137,6 +52,9 @@ def add_candidato():
         print(candidatos)
     btn_add_candidato = tk.Button(janela_add_candidato, text="Salvar", command=confirmar_add_candidato)
     btn_add_candidato.pack(pady=5)
+    # BOTÃO DE VOLTAR
+    btn_voltar = tk.Button(janela_add_candidato, text="voltar", command=janela_add_candidato.destroy)
+    btn_voltar.pack(pady=10)
 
 def votar():
     janela_votacao = tk.Toplevel(janela)
@@ -149,22 +67,35 @@ def votar():
     entrada_voto = tk.Entry(janela_votacao)
     entrada_voto.pack(pady=5)
     def confirmar_voto():
+        candidato_encontrado = False
         matricula = entrada_matricula.get()
         voto = int(entrada_voto.get())
         if matricula:
             for nome, dados in candidatos.items():
                 if dados['numero'] == voto:
-                    confirmacao = messagebox.askyesno('Confirmação', f'Você portador da matricula: {matricula}, confirma seu voto em {nome}, {dados['numero']}, {dados['partido']}')
-                
-
-        # if confirma:
-        #     print(candidatos)
+                    confirmacao = messagebox.askyesno('Confirmação', f'Você portador da matricula: {matricula}, confirma seu voto em {nome}, Número: {dados['numero']}, {dados['partido']}')
+                    dados['voto'] += 1
+                    if confirmacao: 
+                        voto_feito = messagebox.showinfo('Confirmação', 'Voto feito')
+                        # print(candidatos)
+                    candidato_encontrado = True
+                    break
             
-        
-        
+                
+                
+        if not candidato_encontrado:
+            nao_encontrado_candidato = messagebox.showinfo('Erro', 'Candidato não encontrado')
+                
     btn_votar = tk.Button(janela_votacao, text='Confirmar Voto', command=confirmar_voto)
     btn_votar.pack(pady=30)
+    # BOTÃO DE VOLTAR
+    btn_voltar = tk.Button(janela_votacao, text="voltar", command=janela_votacao.destroy)
+    btn_voltar.pack(pady=10)
     
+def imprimir_relatorio():
+    eleito = max(candidatos, key=lambda nome: candidatos[nome]['voto'])
+    print(f'O vendedor da eleição é {eleito}, com {candidatos[eleito]['voto']} votos.')
+        
             
 
 
